375eed51153ebc76bf886cbe5283119ee46d474d
AlexCoilaJrt <alex.coila.jrt@gmail.com>
2024-08-27 14:42:22 -0500

    androisito

---
+++ app/src/main/java/pe/edu/upeu/navegacionjpc/MainActivity.kt
@@ -0,0 +1,57 @@
+package pe.edu.upeu.navegacionjpc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.upeu.navegacionjpc.ui.presentation.components.MyAppDrawer
+import pe.edu.upeu.navegacionjpc.ui.theme.NavegacionJPCTheme
+import pe.edu.upeu.navegacionjpc.ui.theme.ThemeType
+import pe.edu.upeu.navegacionjpc.utils.isNight
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val themeType= remember{ mutableStateOf(ThemeType.RED) }
+            val darkThemex= isNight()
+            val darkTheme = remember { mutableStateOf(darkThemex) }
+
+            NavegacionJPCTheme {
+                MyAppDrawer(darkMode =darkTheme, themeType = themeType )
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }*/
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavegacionJPCTheme {
+        Greeting("Android")
+    }
+}
